9ff94774ac2305f5c0bae38aceb478d44a5ce21b
Matt <matthuntt16@gmail.com>
2020-02-10 20:08:41 -0500

    Added login and logout functionality

--- login/views.py
+++ login/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from django.contrib.auth import logout
+from django.http import HttpResponse, HttpResponseRedirect
+from django.urls import reverse
 
-# Create your views here.
+def index(request):
+    return render(request, 'login/index.html', {})
+
+def signout(request):
+    if request.user.is_authenticated:
+        logout(request)
+        return HttpResponseRedirect(reverse('login:index', args=()))
--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -31,12 +31,18 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'login',
     'django.contrib.admin',
     'django.contrib.auth',
+    'django.contrib.sites',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +124,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+AUTHENTICATION_BACKENDS = (
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
+
+SITE_ID = 1
+
+LOGIN_REDIRECT_URL = '/'
