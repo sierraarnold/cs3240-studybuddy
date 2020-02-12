fbf8f3d3e22db590a6712c7431a5de8d48b3d241
Holly Wikiera <59944490+hwikiera@users.noreply.github.com>
2020-02-12 15:30:02 -0500

    Displaying user on home page

--- login/views.py
+++ login/views.py
@@ -5,6 +5,15 @@ from django.urls import reverse
 from django.shortcuts import get_object_or_404, render
 from django.template import loader
 
+from allauth.account.forms import LoginForm 
+
+"""class MyCustomLoginForm(LoginForm):
+
+    def login(self, *args, **kwargs):
+        # Add your own processing here.
+        # You must return the original result.
+        save_user(self, request, user, form)
+        return super(MyCustomLoginForm, self).login(*args, **kwargs)"""
 
 def home(request):
     template = loader.get_template('login/home.html')
@@ -20,3 +29,5 @@ def signout(request):
     if request.user.is_authenticated:
         logout(request)
         return HttpResponseRedirect(reverse('login:index', args=()))
+
+
