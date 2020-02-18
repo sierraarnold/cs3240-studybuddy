5d98b8c7d950c27cb29cc78c6df0e81703d3a585
Holly Wikiera <59944490+hwikiera@users.noreply.github.com>
2020-02-17 19:36:42 -0500

    first test

--- login/tests.py
+++ login/tests.py
@@ -1,3 +1,17 @@
-from django.test import TestCase
+import datetime
 
-# Create your tests here.
+from django.test import TestCase
+from django.utils import timezone
+from django.urls import reverse
+from .models import Profile
+from .models import TutorCourse
+from .models import StudentCourse
+
+class ProfileModelTests(TestCase):
+#TEST must begin with the string test
+    def test_username_string(self):
+        """
+        input username is returned by to string method
+        """
+        p = Profile(username="holly")
+        self.assertEqual(p.__str__(), "holly")
