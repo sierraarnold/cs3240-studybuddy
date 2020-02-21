168a12e78c14c79790587cfdccfe45ad47b2a35f
Matt <matthuntt16@gmail.com>
2020-02-21 16:49:53 -0500

    fixing service worker

--- login/templates/login/firebase-messaging-sw.js
+++ login/templates/login/firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/7.9.0/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/7.9.0/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/7.8.2/firebase-messaging.js');
 
 var firebaseConfig = {
   apiKey: "AIzaSyAbKG0p096vmi75oPf2ZA5gkOfhTSpOIjk",
