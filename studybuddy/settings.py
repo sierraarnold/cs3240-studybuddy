99c1f2ef89f00314f8c057059146c9b5ed0e8ae0
Holly Wikiera <59944490+hwikiera@users.noreply.github.com>
2020-02-25 17:45:12 -0500

    i

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import django_heroku
+import dj_database_url
 #try:
 #    import django_heroku
 #    django_heroku.settings(locals())
