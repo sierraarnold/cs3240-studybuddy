e8110083d8ede8d68e6453b618e417826a89ce6e
Holly Wikiera <59944490+hwikiera@users.noreply.github.com>
2020-02-25 17:21:36 -0500

    settings

--- studybuddy/settings.py
+++ studybuddy/settings.py
@@ -11,11 +11,12 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-try:
-    import django_heroku
-    django_heroku.settings(locals())
-except ImportError:
-    found = False
+import django_heroku
+#try:
+#    import django_heroku
+#    django_heroku.settings(locals())
+#except ImportError:
+ #   found = False
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
